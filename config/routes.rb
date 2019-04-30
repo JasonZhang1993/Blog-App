cd8c0b47a9624bd3979926ff71a26344c2b70bb6
Jiayi Zhang <jiayizha@amazon.com>
2019-04-30 17:43:56 +0800

    Add Welcome and Articles controller. Done at 5.2
    https://guides.rubyonrails.org/getting_started.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  # map requests to http://localhost:3000/welcome/index to the welcome controller's index action
+  get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Rails provides a resources method which can be used to declare a standard REST resource.
+  # Check this by running `bin/rails routes`
+  resources :articles
+
+  # map requests to the root of the application to the welcome controller's index action
+  root 'welcome#index'
 end
