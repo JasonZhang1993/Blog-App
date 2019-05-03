9adabdacc600098a22c6f74db2c784259a5f73f8
JasonZhang1993 <jiayi.zhang1993@gmail.com>
2019-05-03 15:48:35 +0800

    Create Article.create action. Done 5.3

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
   def new
   end
+
+  def create
+    # render method here is taking a very simple hash with a key of :plain and value of params[:article].inspect
+    render plain: params[:article].inspect
+  end
 end
