c75881c3485abffd4a8c35ef3b17c1e926afcefb
JasonZhang1993 <jiayi.zhang1993@gmail.com>
2019-05-04 14:50:49 +0800

    Done at 5.13.
    Index:  list all articles, add links between action views
    new:    Add validation
    edit:   Add edit and update actions with validation, refactor html views with partials
    delete: Add delete action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+  # A frequent practice is to place the standard CRUD actions in each controller in the following order: index, show, new, edit, create, update and destroy.
+  def index
+    @articles = Article.all
+  end
+
   def show
     # use Article.find to find the article we are interested in. Passing [:id] parameter to get the :id from the request
     # An instance variable @article is referenced to the article object
@@ -6,6 +11,12 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    # 5.10 render a new instance
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -16,8 +27,29 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     # save the model to database, redirect user to show action
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      # 5.10 if save return false because of validation fail, show the form back to user and render a new instance
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   private
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+  # Active Record Validation: https://guides.rubyonrails.org/active_record_validations.html
+  validates :title, presence: true,
+                    length: { minimum: 5}
 end
