1681f68c403ad4def82ae9c9b27de3c9056158b9
Jiayi Zhang <jiayizha@amazon.com>
2019-05-05 14:40:24 +0800

    Done at 8. Add destroy action for Comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
   # Associate with Comment model: an article can have many comments
-  has_many :comments
+  # use the dependent option of an association to delete all comments if the associated article is deleted
+  has_many :comments, dependent: :destroy
   # Active Record Validation: https://guides.rubyonrails.org/active_record_validations.html
   validates :title, presence: true,
                     length: { minimum: 5}
